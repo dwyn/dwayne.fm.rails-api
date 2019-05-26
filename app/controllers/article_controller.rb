dec419c27257a51ebc2f4a7b4d29c3a02b022606
dwieb <dwayne.harmon@gmail.com>
2019-05-26 14:21:38 -0500

    Article index is now displaying an array of json objects.
      - Did you know all insects have 6 legs?

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,7 +2,7 @@ class ArticleController < ApplicationController
 
   def index
     @articles = Article.select("id, title").all
-    render json: @aticles.to_json
+    render json: @articles.to_json
   end
 
 end
