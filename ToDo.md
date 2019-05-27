91801f6c934766d56227d3e592465ed53388afc6
dwieb <dwayne.harmon@gmail.com>
2019-05-27 05:06:35 -0500

    Updated websie-config.ts
      - Did you know 96% of candles sold are purchased by women? (I buy
      candles...)

---
+++ ToDo.md
@@ -0,0 +1,4 @@
+Next up
+
+1 Connect Google Analytics
+2 Connect MailChimp
--- client/src/website-config.ts
+++ client/src/website-config.ts
@@ -29,14 +29,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'dwayne.fm',
+  description: 'Music, musings and modern hieroglyphs.',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://dwayne.fm',
+  facebook: 'https://www.facebook.com/',
+  twitter: 'https://twitter.com/dwyn',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
