631cf2235f3caa91d1c9b7f4070cc4860b4af262
dwieb <dwayne.harmon@gmail.com>
2019-07-17 22:28:24 -0500

    Set up rails to play nicely with React-Router
      - If implemented properly I should have some nice 404 error handling
      - Did you know grapes light on fire in the microwave?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+
+  # Rails to pass any HTML requests that it doesnt catch to React
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
     resources :projects, only: [:index, :show]
   end
   
+  # Rails to pass any HTML requests that it doesnt catch to React
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
