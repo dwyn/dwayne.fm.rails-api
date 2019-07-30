69813266a70857e5a99e7191b3079f8a7317940e
dwieb <dwayne.harmon@gmail.com>
2019-07-30 04:27:45 -0500

    Add a footer. Yay for footers. Amirite?
      - Did you know all the blinking in one day equates to having your eyes
      closed for 30 minutes.

---
+++ client/src/containers/FooterContainer.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+// import ___ from '../components/___.jsx'
+
+
+var style = {
+  backgroundColor: "#100431",
+  color: "#bf928a",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "55px",
+  width: "100%",
+};
+
+class FooterContainer extends Component {
+  render() {
+    return (
+      <div className="NavigationContainer" style={style}>
+        <p>footer?</p>
+      </div>
+    )
+  }
+}
+
+export default FooterContainer;
